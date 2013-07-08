539b15f4784689b5fc49f775f11369cfeaa8b71b
Jingwen Owen Ou <jingweno@gmail.com>
2013-07-07 23:30:49 -0700

    Add builtin functions

--- main.go
+++ main.go
@@ -7,14 +7,49 @@ import (
 	"os"
 	"os/exec"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
+type Builtin struct {
+	Run func(args []string)
+}
+
+var BUILTINS = map[string]*Builtin{
+	"cd": &Builtin{func(args []string) {
+		os.Chdir(args[0])
+	}},
+	"exit": &Builtin{func(args []string) {
+		var code int
+		if len(args) == 1 {
+			code, _ = strconv.Atoi(args[0])
+		}
+		os.Exit(code)
+	}},
+	"exec": &Builtin{func(args []string) {
+		spawnProgram(args[0], args[1:])
+	}},
+	"set": &Builtin{func(args []string) {
+		for _, arg := range args {
+			keyValuePair := strings.Split(arg, "=")
+			if len(keyValuePair) == 2 {
+				os.Setenv(keyValuePair[0], keyValuePair[1])
+			}
+		}
+	}},
+}
+
 func main() {
+	os.Setenv("PROMPT", "->")
 	prompt()
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		cmd := parseCmd(scanner.Text())
-		spawnProgram(cmd[0], cmd[1:])
+		name, args := parseCmd(scanner.Text())
+		if isBuiltin(name) {
+			callBuiltin(name, args)
+		} else {
+			spawnProgram(name, args)
+		}
 		prompt()
 	}
 
@@ -24,12 +59,28 @@ func main() {
 }
 
 func prompt() {
-	fmt.Fprint(os.Stdout, "-> ")
+	fmt.Fprintf(os.Stdout, "%s ", os.Getenv("PROMPT"))
 }
 
-func parseCmd(text string) []string {
+func parseCmd(text string) (name string, args []string) {
 	regexpBySpace := regexp.MustCompile("\\s+")
-	return regexpBySpace.Split(text, -1)
+	cmd := regexpBySpace.Split(text, -1)
+
+	name = cmd[0]
+	args = cmd[1:]
+
+	return
+}
+
+func isBuiltin(name string) bool {
+	_, ok := BUILTINS[name]
+
+	return ok
+}
+
+func callBuiltin(name string, args []string) {
+	builtin, _ := BUILTINS[name]
+	builtin.Run(args)
 }
 
 func spawnProgram(name string, args []string) {
